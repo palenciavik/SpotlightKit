445eb23fd72f710d41ecb72ca61e06022e1a5c3f
palenciaVik <palenciavik@gmail.com>
2025-02-10 19:39:52 -0800

    Revert package.swift changes

--- Package.swift
+++ Package.swift
@@ -16,7 +16,13 @@ let package = Package(
     targets: [
         .target(
             name: "SpotlightKit",
-        )
+            path: "Sources/SpotlightKit"
+        ),
+        .testTarget(
+            name: "SpotlightKitTests",
+            dependencies: ["SpotlightKit"],
+            path: "Tests/SpotlightKitTests"
+        ),
     ]
 )
 
